c0a645f7869cad2a9cd74c70575aaabf0172b2bc
Souhayl Maronesy <smaronesy@Souhayls-MacBook-Air.local>
2023-01-06 16:57:42 -0800

    refactured the code

--- app/src/main/java/com/example/sportresults/ui/results/ResultsListFragment.kt
+++ app/src/main/java/com/example/sportresults/ui/results/ResultsListFragment.kt
@@ -9,8 +9,6 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sportresults.databinding.FragmentResultsListBinding
-import com.example.sportresults.utils.convertLongToTime
-import com.example.sportresults.utils.getDateOnly
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ResultsListFragment : Fragment() {
@@ -48,7 +46,7 @@ class ResultsListFragment : Fragment() {
             if(it != null){
                 val foneLatest = it.winner + " wins " + it.tournament + " by " +
                         it.seconds + " seconds"
-                resultsSetHelper(it.publicationDate, resultList, foneLatest)
+                resultViewModel.resultsSetHelper(it.publicationDate, resultList, foneLatest)
             }
         })
 
@@ -56,7 +54,7 @@ class ResultsListFragment : Fragment() {
             if(it != null){
                 val nbaLatest = it.mvp + " leads " + it.winner + " to game "+
                         it.gameNumber + " win in the " + it.tournament
-                resultsSetHelper(it.publicationDate, resultList, nbaLatest)
+                resultViewModel.resultsSetHelper(it.publicationDate, resultList, nbaLatest)
             }
         })
 
@@ -64,7 +62,7 @@ class ResultsListFragment : Fragment() {
             if(it != null){
                 val tennisLatest = it.tournament + ": " + it.winner + " wins against " + it.looser +
                         " in " + it.numberOfSets + " sets"
-                resultsSetHelper(it.publicationDate, resultList, tennisLatest)
+                resultViewModel.resultsSetHelper(it.publicationDate, resultList, tennisLatest)
             }
         })
 
@@ -75,33 +73,4 @@ class ResultsListFragment : Fragment() {
         return resultListBinding.root
     }
 
-    private fun resultsSetHelper(
-        publicationDate: Long,
-        resultList: MutableMap<Long, LinkedHashSet<Pair<Long, String>>>,
-        foneLatest: String
-    ) {
-        val dateOnly = getDateOnly(publicationDate)
-        if (resultList.contains(dateOnly)) {
-            resultList[dateOnly]!!.add(Pair(publicationDate, foneLatest))
-        } else {
-            resultList[dateOnly] = linkedSetOf(Pair(publicationDate, foneLatest))
-        }
-
-        // includes only the results from the most recent date
-        val latest = resultList.keys.max()
-        resultViewModel.mostRecentDate.value = convertLongToTime(latest)
-
-        // sort results by date
-        val resultsSortedByDate = sortByDate(resultList[latest]!!)
-        resultViewModel.latestResults.value = resultsSortedByDate
-    }
-
-    private fun sortByDate(set: LinkedHashSet<Pair<Long, String>>): LinkedHashSet<String>{
-        val sorted = set.sortedByDescending { it.first }
-        val sortedResult = linkedSetOf<String>()
-        for(r in sorted){
-            sortedResult.add(r.second)
-        }
-        return sortedResult
-    }
 }
--- app/src/main/java/com/example/sportresults/ui/results/ResultsViewModel.kt
+++ app/src/main/java/com/example/sportresults/ui/results/ResultsViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.sportresults.repository.ResultsRepository
+import com.example.sportresults.utils.convertLongToTime
+import com.example.sportresults.utils.getDateOnly
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
@@ -29,6 +31,36 @@ class ResultsViewModel(application: Application, private val resultsRepository: 
         viewModelScope.launch {
             resultsRepository.refreshResults()
         }
+    }
+
+    fun resultsSetHelper(
+        publicationDate: Long,
+        resultList: MutableMap<Long, LinkedHashSet<Pair<Long, String>>>,
+        foneLatest: String
+    ) {
+        val dateOnly = getDateOnly(publicationDate)
+        if (resultList.contains(dateOnly)) {
+            resultList[dateOnly]!!.add(Pair(publicationDate, foneLatest))
+        } else {
+            resultList[dateOnly] = linkedSetOf(Pair(publicationDate, foneLatest))
+        }
+
+        // includes only the results from the most recent date
+        val latest = resultList.keys.max()
+        mostRecentDate.value = convertLongToTime(latest)
+
+        // sort results by date
+        val resultsSortedByDate = sortByDate(resultList[latest]!!)
+        latestResults.value = resultsSortedByDate
+    }
+
+    private fun sortByDate(set: LinkedHashSet<Pair<Long, String>>): LinkedHashSet<String>{
+        val sorted = set.sortedByDescending { it.first }
+        val sortedResult = linkedSetOf<String>()
+        for(r in sorted){
+            sortedResult.add(r.second)
+        }
+        return sortedResult
     }
 
 }
